84efab7d1c349941ba07d81a2d3406bd65623e63
stha <122188963+stha1122@users.noreply.github.com>
2023-07-18 23:11:13 +0530

    Add files via upload

--- Recursion/recursion_problems/firtsOccurence.java
+++ Recursion/recursion_problems/firtsOccurence.java
@@ -1,10 +1,17 @@
 package recursion_problems;
 
+import java.util.Scanner;
+
 public class firtsOccurence {
 
 	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
 		int arr[]= {1,2,3,2,4,5,6};
 		System.out.println(	firstOccurence(arr,2,0));
+		int num=s.nextInt();
+		int index=-1;
+		int start=0;
+		System.out.println(findfirst(start,arr,num,index));
 	
 	}
 	public static int firstOccurence(int arr[],int num ,int index) {
@@ -19,5 +26,15 @@ public class firtsOccurence {
 	   
 		
 	}
-
+	private static int findfirst(int start, int[] arr, int num, int index) {
+		if(start>=arr.length) {
+			return index;
+		}
+		if(num==arr[start]) {
+			index=start;
+			return index;
+		}
+		index=findfirst(start+1,arr,num,index);
+		return index;
+	}
 }
