dafe4b0fcbcb839570ca296e24991acc5b1e6346
stha <122188963+stha1122@users.noreply.github.com>
2023-09-06 22:54:36 +0530

    Update Last_Index.java

--- Recursion/recursion_problems/Last_Index.java
+++ Recursion/recursion_problems/Last_Index.java
@@ -9,7 +9,7 @@ public class Last_Index {
 		Scanner s = new Scanner(System.in);
 		int n =s.nextInt();
 		int arr[]=new int[n];
-		for(int i=0;i<n;i++) {
+		for(int i=0;i<=n;i++) {
 			arr[i]=s.nextInt();
 		}
 		int num=s.nextInt();
